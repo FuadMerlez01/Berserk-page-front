c241732f6a1e1c4f4ff05d60150de969a7eb7e5b
FuadMerlez01 <146130984+FuadMerlez01@users.noreply.github.com>
2024-07-04 01:57:19 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+const idiomasElements = document.getElementById("idiomas");
+const textsToChange = document.querySelectorAll("[data-section]");
+const card = document.querySelector(".card")
+
+function calculateAngle(x1, y1, x2, y2) {
+  const dy = y2 - y1;
+  const dx = x2 - x1;
+  const rad = Math.atan2(dy, dx); // Radians
+  const deg = rad * (180 / Math.PI); // Degrees
+  return deg;
+}
+
+window.addEventListener("mousemove", (e) => {
+  const rect = card.getBoundingClientRect()
+  const angle = calculateAngle(rect.x, rect.y, e.offsetX, e.offsetY)
+  card.style.setProperty("--angle", angle + "deg");
+});
+
+
+const changeLanguage = async (language) => {
+    try {
+        const requestJson = await fetch(`language/${language}.json`);
+        if (!requestJson.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const texts = await requestJson.json();
+        
+        // Aquí puedes utilizar los textos cargados del JSON
+        console.log(`Contenido del archivo ${language}.json:`);
+        
+        textsToChange.forEach(text => {
+            const section = text.dataset.section;
+            const value = text.dataset.value;
+
+            text.innerHTML = texts[section][value];
+        });
+        
+        // Ejemplo: Actualización de contenido basado en el idioma
+        // updateContent(texts);
+    } catch (error) {
+        console.error('Error fetching language JSON:', error);
+    }
+};
+
+idiomasElements.addEventListener("click", (e) => {
+    const language = e.target.closest("a").dataset.language;
+    
+    if (language) {
+        changeLanguage(language);
+    }
+});
+
+
+
+
+
